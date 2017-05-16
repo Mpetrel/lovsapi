cc196a2bb966b710294b16869af78e1301f5b513
mpetrel <mmpetrel@live.com>
2017-05-16 22:30:14 +0800

    close connect

--- app.py
+++ app.py
@@ -7,8 +7,10 @@
 # @Software: PyCharm Community Edition
 from flask import Flask
 from route.mzt import mzt
+from route.my_response import MyResponse
 
 app = Flask(__name__)
+app.response_class = MyResponse
 app.register_blueprint(mzt, url_prefix='/mzt/v1')
 
 @app.route("/")
